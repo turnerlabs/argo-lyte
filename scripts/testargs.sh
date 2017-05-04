c87d6758c13f1d0384070b8d9495be5a7793561e
Michael Smith <smithatlanta@gmail.com>
2017-05-03 20:56:05 -0400

    upped version

--- scripts/testargs.sh
+++ scripts/testargs.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-wget https://github.com/turnerlabs/argo-lyte/blob/master/releases/0.0.2/argo-lyte-linux-amd64
+wget https://github.com/turnerlabs/argo-lyte/blob/master/releases/0.0.3/argo-lyte-linux-amd64
 chmod 700 /home/vagrant/argo-lyte-linux-amd64
 sudo /home/vagrant/argo-lyte-linux-amd64 -workdirectory="${WORK_DIR}" -userurl="${USER_URL}"
