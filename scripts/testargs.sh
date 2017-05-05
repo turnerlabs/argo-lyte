08083a92fad53a081ae434f6a10141a6c34e1d06
Michael Smith <smithatlanta@gmail.com>
2017-05-05 10:22:12 -0400

    This should work but alas it does not.

--- scripts/testargs.sh
+++ scripts/testargs.sh
@@ -5,4 +5,10 @@ set -x
 
 wget -qO- https://github.com/turnerlabs/argo-lyte/releases/download/0.0.3/argo-lyte-linux-amd64 >> argo-lyte-linux-amd64
 chmod 700 /home/vagrant/argo-lyte-linux-amd64
-sudo /home/vagrant/argo-lyte-linux-amd64 -workdirectory="${WORK_DIR}" -userurl="${USER_URL}"
+sudo /home/vagrant/argo-lyte-linux-amd64 -workdirectory=${WORK_DIR} -userurl=${USER_URL}
+
+echo '#!/bin/bash' >> /home/vagrant/argo-lyte
+command="sudo /home/vagrant/argo-lyte-linux-amd64 -workdirectory=${WORK_DIR} -userurl=${USER_URL}"
+echo $command >> /home/vagrant/argo-lyte
+chmod 755 /home/vagrant/argo-lyte
+sudo cp /home/vagrant/argo-lyte /etc/cron.hourly/argo-lyte
