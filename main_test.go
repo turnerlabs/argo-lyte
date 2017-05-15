9800e080fd3bd9d171465bd0e5f2ff265fb6b192
Michael Smith <smithatlanta@gmail.com>
2017-05-15 13:21:14 -0400

    Fixed test

--- main_test.go
+++ main_test.go
@@ -8,16 +8,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// FYI, The bin group is not always 7
 func TestGetGIDByGroupName(t *testing.T) {
-	result := getGIDByGroupName("bin")
-	assert.Equal(t, result, 7)
+	result := getGIDByGroupName("wheel")
+	assert.Equal(t, result, 0)
 }
 
-// FYI, The _timezone user is not always 210
 func TestGetUIDByUserName(t *testing.T) {
-	result := getUIDByUserName("_timezone")
-	assert.Equal(t, result, 210)
+	result := getUIDByUserName("root")
+	assert.Equal(t, result, 0)
 }
 
 // Test the byte array stuff
